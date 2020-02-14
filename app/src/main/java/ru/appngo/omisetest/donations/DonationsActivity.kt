18d929d51860a8d4a293ac78779bc3a24bada99f
aasavchenko <andrey.savchenko@tala.co>
2020-02-14 09:59:20 +0300

    add DonationActivity logic

--- app/src/main/java/ru/appngo/omisetest/donations/DonationsActivity.kt
+++ app/src/main/java/ru/appngo/omisetest/donations/DonationsActivity.kt
@@ -1,12 +1,21 @@
 package ru.appngo.omisetest.donations
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_donations.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import ru.appngo.omisetest.R
 import ru.appngo.omisetest.charities.data.Charity
+import ru.appngo.omisetest.donations.data.Donation
+import ru.appngo.omisetest.network.Services
 
 private const val DONATION_NAME_EXTRA = "charity"
 
@@ -20,27 +29,43 @@ class DonationsActivity : AppCompatActivity(), DonationsContract.View {
                 }
     }
 
+    private val presenter: DonationsContract.Presenter = DonationsPresenter(Services.getOmiseService())
+    private val progressDialog: ProgressDialog by lazy {
+        ProgressDialog(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donations)
+        presenter.onStart(this)
         donation_name.text = getDonationName()
+        donate_button.setOnClickListener {
+            GlobalScope.launch(context = Dispatchers.IO) {
+                // TODO: can also add checks on empty fields, but don't think it worth it in the test project.
+                presenter.onSendButtonClick(
+                        Donation(name = donation_name.text.toString(),
+                                token = account_number.text.toString(),
+                                amount = amount_to_donate.text.toString().toLong()))
+            }
+        }
     }
 
     private fun getDonationName() = intent.getStringExtra(DONATION_NAME_EXTRA)
 
-    override suspend fun showProgressIndicator() {
-
+    override suspend fun showProgressIndicator() = withContext(Main) {
+        progressDialog.show()
     }
 
-    override suspend fun hideProgressIndicator() {
-
+    override suspend fun hideProgressIndicator() = withContext(Main) {
+        progressDialog.dismiss()
     }
 
-    override suspend fun navigateToSuccess() {
-
+    override suspend fun navigateToSuccess() = withContext(Main) {
+        // TODO: should be navigation to success screen,
+        //  but service provides only error message, so cannot be implemented yet.
     }
 
-    override suspend fun showError(error: String) {
-
+    override suspend fun showError(error: String) = withContext(Main) {
+        Toast.makeText(this@DonationsActivity, error, Toast.LENGTH_SHORT).show()
     }
 }
